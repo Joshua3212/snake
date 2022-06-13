3c90cd9a45d71bf7484f32c324bc15d23dc12222
Joshua <joshua-koeck@gmx.de>
2022-06-13 22:18:00 +0200

    rendering *something*

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -2,15 +2,17 @@ package org.example;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameListener;
-import org.example.screens.gameScreen;
+import de.gurkenlabs.litiengine.resources.Resources;
+import org.example.screens.IngameScreen;
 
 public class Program {
     public static void main(String[] args) {
-        Game.init(args);
-        Game.start();
 
-
+        // configure the game
         //https://litiengine.com/docs/game-api/
+
+        Game.init(args);
+
 
         Game.addGameListener(new GameListener() {
             @Override
@@ -26,6 +28,14 @@ public class Program {
 
         });
 
-        Game.screens().add(new gameScreen());
+        // load resources
+        Resources.load("game.litidata");
+        Game.world().loadEnvironment("level1");
+
+        Game.graphics().setBaseRenderScale(4f);
+
+        Game.screens().add(new IngameScreen());
+
+        Game.start();
     }
 }
